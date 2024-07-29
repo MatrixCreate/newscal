6e0a623edbee311fa1b7d7bbb8b811c3266ae062
Matthew Ewan-Laws <matthew.ewan-laws@newsquest.co.uk>
2024-07-29 15:04:38 +0100

    add calendar

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import DataTable from 'datatables.net-dt';
 import 'datatables.net-responsive-dt';
+import { Calendar } from '@fullcalendar/core';
+
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 window.addEventListener('DOMContentLoaded', () => {
-  let table = new DataTable('#myTable', {
-      responsive: true
-  });
+  if (document.querySelector('#myTable')) {
+    let table = new DataTable('#myTable', {
+        responsive: true
+    });
+  }
 
+  const calendarEl = document.querySelector('.calendar')
+
+  if (calendarEl) {
+    console.log(window.calEvents)
+    const calendar = new Calendar(calendarEl, {
+      plugins: [ dayGridPlugin ],
+      viewType: "dayGridMonth",
+      events:  [...window.calEvents, ],
+    })
+
+    calendar.render()
+  }
 })

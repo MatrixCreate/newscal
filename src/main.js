198bf319abc28907ae24d3b97e9e1f39f5fdc06a
Matthew Ewan-Laws <matthew.ewan-laws@newsquest.co.uk>
2024-07-29 17:08:50 +0100

    feat: allow user to select the source

--- src/main.js
+++ src/main.js
@@ -4,13 +4,48 @@ import { Calendar } from '@fullcalendar/core';
 
 import dayGridPlugin from '@fullcalendar/daygrid';
 
+
 window.addEventListener('DOMContentLoaded', () => {
-  if (document.querySelector('#myTable')) {
-    let table = new DataTable('#myTable', {
+  if (document.querySelector('#eventsTable')) {
+    let table = new DataTable('#eventsTable', {
         responsive: true
     });
-  }
 
+    const filteredSources = new Set()
+
+    document.querySelectorAll('.js-source').forEach(sourceEl => {
+      const updateSourceEl = () => {
+        const name = sourceEl.value
+
+        if (sourceEl.checked) {
+          filteredSources.delete(name)
+        } else {
+          filteredSources.add(name)
+        }
+
+        updateSearch(table, filteredSources)
+      }
+
+      updateSourceEl()
+      updateSearch(table, filteredSources)
+
+      sourceEl.addEventListener('change', () => {
+        updateSourceEl()
+        updateSearch(table, filteredSources)
+      })
+    })
+  }
+})
+
+
+function updateSearch(table, filteredSources) {
+  table
+    .column(1)
+    .search(text => !filteredSources.has(text))
+    .draw()
+}
+
+window.addEventListener('DOMContentLoaded', () => {
   const calendarEl = document.querySelector('.calendar')
 
   if (calendarEl) {

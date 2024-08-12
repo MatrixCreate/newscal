5d8d91ac0b2a7f8bd218ef09b077cda3827fa1a9
Matthew Ewan-Laws <matthew.ewan-laws@newsquest.co.uk>
2024-08-12 14:44:21 +0100

    allow hiding of future events + prettier

--- src/main.js
+++ src/main.js
@@ -1,84 +1,91 @@
 import DataTable from 'datatables.net-dt';
 import 'datatables.net-responsive-dt';
 import { Calendar } from '@fullcalendar/core';
-
 import dayGridPlugin from '@fullcalendar/daygrid';
 
-const SOURCE_COL = 2
-const DATE_COL = 3
+const SOURCE_COL = 2;
+const DATE_COL = 3;
 
 function isInThePast(dateString) {
   const today = new Date();
   today.setHours(0, 0, 0, 0); // Reset time to 00:00:00
 
-  const year = dateString.split('-')[0]
   const inputDate = new Date(dateString + 'T00:00:00'); // Parse input date
-  if (year == '2023') {
-    console.log(inputDate < today)
-  }
 
   return inputDate < today; // Compare dates
 }
 
-
 window.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('#eventsTable')) {
     let table = new DataTable('#eventsTable', {
-        responsive: true
+      responsive: true
     });
 
-    const pastEventsEl = document.querySelector('.js-past-events')
+    const pastEventsEl = document.querySelector('.js-past-events');
+    const hideFutureEventsEl = document.querySelector('.js-future-events');
+
+    const updateOption = () => {
+      const showPast = pastEventsEl.checked;
+      const hideFuture = hideFutureEventsEl.checked;
+      //console.log({ showPast, hideFuture })
+
+      table.column(DATE_COL).search((dateStr) => {
+        const isPast = isInThePast(dateStr);
+        if (showPast && hideFuture) { //
+          // console.log({ dateStr, isPast })
+          return isPast;
+        } else if (showPast && !hideFuture) { // everything
+          return true;
+        } else if (!showPast && hideFuture) {
+          return false;
+        } else { // (!showPast && !hideFuture)
+          return !isPast; // Show all if neither option is selected
+        }
+      }).draw();
+    };
 
     if (pastEventsEl) {
-      const updateOption = () => {
-        const shouldShowPast = pastEventsEl.checked
-        if (shouldShowPast) {
-          table.column(DATE_COL).search((text) => true).draw()
-        } else {
-          table.column(DATE_COL).search((dateStr) => !isInThePast(dateStr)).draw()
-        }
-      }
-
-      updateOption()
-
-      pastEventsEl.addEventListener('change', (e) => {
-        updateOption()
-      })
+      updateOption();
+      pastEventsEl.addEventListener('change', updateOption);
     }
 
-    const filteredSources = new Set()
+    if (hideFutureEventsEl) {
+      hideFutureEventsEl.addEventListener('change', updateOption);
+    }
+
+    const filteredSources = new Set();
 
     document.querySelectorAll('.js-source').forEach(sourceEl => {
       const updateSourceEl = () => {
-        const name = sourceEl.value
+        const name = sourceEl.value;
 
         if (sourceEl.checked) {
-          filteredSources.delete(name)
+          filteredSources.delete(name);
         } else {
-          filteredSources.add(name)
+          filteredSources.add(name);
         }
 
-        updateSearch(table, filteredSources)
-      }
+        updateSearch(table, filteredSources);
+      };
 
-      updateSourceEl()
-      updateSearch(table, filteredSources)
+      updateSourceEl();
+      updateSearch(table, filteredSources);
 
       sourceEl.addEventListener('change', () => {
-        updateSourceEl()
-        updateSearch(table, filteredSources)
-      })
-    })
+        updateSourceEl();
+        updateSearch(table, filteredSources);
+      });
+    });
   }
-})
-
+});
 
 function updateSearch(table, filteredSources) {
   table
     .column(SOURCE_COL)
     .search(text => !filteredSources.has(text))
-    .draw()
+    .draw();
 }
+
 
 window.addEventListener('DOMContentLoaded', () => {
   const calendarEl = document.querySelector('.calendar')
